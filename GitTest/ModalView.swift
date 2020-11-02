eba0cbad009c4ac12cc6ab85aee25907f9022691
Ivan Dosev <biobeehive@abv.bg>
2020-11-02 17:00:01 +0100

    New modal view

---
+++ GitTest/ModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ModalView.swift
+//  GitTest
+//
+//  Created by Ivan Dimitrov on 2.11.20.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text("This is the  modal view")
+    }
+}
+
+struct ModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ModalView()
+    }
+}
