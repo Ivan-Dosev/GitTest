a1517cc8c7350c8abde5740df5c31a5a3f620f5a
Ivan Dosev <biobeehive@abv.bg>
2020-11-02 16:49:41 +0100

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Ivan Dimitrov on 2.11.20.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
